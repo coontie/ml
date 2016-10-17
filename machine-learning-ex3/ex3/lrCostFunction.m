07618df477865f11c098f519dffe063a45c23aab
Igz <you@example.com>
2016-10-16 23:07:24 -0400

    Finished

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -37,7 +37,12 @@ grad = zeros(size(theta));
 %
 
 
+h = sigmoid(X*theta);
+theta1 = [0 ; theta(2:end, :)];
+p = lambda*(theta1'*theta1)/(2*m);
+J = ((-y)'*log(h) - (1-y)'*log(1-h))/m + p;
 
+grad = (X'*(h - y)+lambda*theta1)/m;
 
 
 
